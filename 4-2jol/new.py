0076690a7739e1b7386c47387c184a3593232290
bokseongbeom <81092350+bokseongbeom@users.noreply.github.com>
2021-09-04 17:02:36 +0900

    Update new.py

--- 4-2jol/new.py
+++ 4-2jol/new.py
@@ -1,9 +1,11 @@
 import requests
 import json
-import openpyxl
 import pandas as pd
+from pandas import json_normalize
+
 import collections
 import folium
+import openpyxl
 
 class KakaoLocalAPI:
     """
@@ -51,63 +53,18 @@ class KakaoLocalAPI:
 
         return document
 
-    def get_keyword_list(self, query, x, y, offset = 0.01):
-        '''
-        계산부분
-        '''
-        page = 1
-        res_list = []
-        abc = offset
-
-        while True:
-            search_count = self.search_keyword(query, x, y, offset=abc)['meta']['total_count']
-            #print(self.search_keyword(query, x, y, offset= offset, page=page))
-            #print(search_count, abc, x, y)
-
-            if search_count > 45:
-                abc = abc/2
-                x_end = x+abc
-                y_end = y+abc
-                a = self.get_keyword_list(query, x, y, offset=abc)
-                b = self.get_keyword_list(query, x_end, y, offset=abc)
-                c = self.get_keyword_list(query, x, y_end, offset=abc)
-                d = self.get_keyword_list(query, x_end, y_end, offset=abc)
-                res_list.extend(a)
-                res_list.extend(b)
-                res_list.extend(c)
-                res_list.extend(d)
-
-                return res_list
-
-            else:
-                if self.search_keyword(query, x, y, offset= abc, page=page)['meta']['is_end']:
-                    res_list.extend(self.search_keyword(query, x, y, offset= abc, page=page)['documents'])
-                    return res_list
-                # 아니면 다음 페이지로 넘어가서 데이터 저장
-                else:
-                    res_list.extend(self.search_keyword(query, x, y, offset= abc, page=page)['documents'])
-                    page += 1
-
-    def search_all(self, query):
-        # 126 - 128 36.5 - 38.5
-        x = 126
-        df = pd.DataFrame()
-
-        for j in range(1, 3):
-            y = 36.5
-            for k in range(1, 3):
-                print(x, y)
-                every_list_one = self.get_keyword_list(query, x, y, offset=0.5)
-                df.append(every_list_one)
-                
-                y += 0.5
-            x += 0.5
-
-        return every_list
-
 # 함수 실행
 query = '순두부'
 kakao = KakaoLocalAPI()
-df = kakao.search_all(query)
-print(df)
+df = pd.json_normalize(kakao.search_keyword(query, x = 127, y = 37.5, page = 1, offset = 0.5 )['documents'])
+df1 = pd.json_normalize(kakao.search_keyword(query, x = 127, y = 36.5, page = 1, offset = 0.5 )['documents'])
+MergedDF = df.append(df1, ignore_index=True)
+print(MergedDF)
 
+
+#df.append(pd.read_json(self.search_keyword(query, x, y, offset= abc, page=page)))
+
+'''
+def search_keyword(self, query, x, y, offset=None, page=None, category_group_code=None,
+                       radius=None, size=None, sort=None):
+'''
